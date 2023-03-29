6fa621664d14f93a81c4e5f92c5422293b2cf529
defarrel <Defarrel>
2023-03-29 13:32:18 +0700

    Membuat parameter

--- ExerciseProsedural_033/ExerciseProsedural_033.cpp
+++ ExerciseProsedural_033/ExerciseProsedural_033.cpp
@@ -1,3 +1,14 @@
 #include <iostream>
 using namespace std;
 
+float ratarata(float a, float b) {
+	return (a * b) / 2;
+}
+
+string status(float r, float n) {
+	if (r >= 70 or n >= 80) {
+		return "Diterima";
+	}else
+		return "Ditolak";
+}
+
