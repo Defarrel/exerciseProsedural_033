9317fe261d27d0242e19376adb48bef71d9c23fb
defarrel <Defarrel>
2023-03-29 14:26:27 +0700

    Memberankan sedikit kode karena tadi tidak jalan dan mencobanya dengan array yang berisi hanya 2

--- ExerciseProsedural_033/ExerciseProsedural_033.cpp
+++ ExerciseProsedural_033/ExerciseProsedural_033.cpp
@@ -1,60 +1,70 @@
 #include <iostream>
 using namespace std;
 
-float ratarata(float a, float b) {
-	return (a * b) / 2;
+
+double ratarata(double a, double b) {
+    return (a + b) / 2;
+
 }
 
-string status(float r, float n) {
-	if (r >= 70 or n >= 80) {
-		return "Diterima";
-	}else
-		return "Ditolak";
+string status(double r, double n) {
+    if (r >= 70 or n >= 80) {
+        return "Diterima";
+    }
+    else {
+        return "Ditolak";
+    }
 }
 
 string nama[20];
-float nilM[20], nilB[20], rataNil[20];
-int jumlahditerima = 0;
-int jumlahditolak = 0;
+double nilM[20], nilB[20], rataNil[20];
 
-void input() {
-	int i = 0;
-	do {
-		cout << "Masukan nama Mahasiswa: ";
-		cin >> nama[i];
-		cout << "Masukan nilai matematika: ";
-		cin >> nilM[i];
-		cout << "Masukan nilai B. Inggris: ";
-		cin >> nilB[i];
+void inputdata() {
+    int i = 0;
+    do {
+        cout << "Masukkan Nama Siswa : ";
+        cin >> nama[i];
+        cout << "Masukkan Nilai Matematika : ";
+        cin >> nilM[i];
+        cout << "Masukkan Nilai Bahasa Inggris : ";
+        cin >> nilB[i];
 
-		cout << ratarata(nilM[i], nilB[i]);
-		i++;
-	} while (i < 20);
+        rataNil[i] = ratarata(nilM[i], nilB[i]);
+        i++;
+    } while (i < 20);
 }
 
+
 void tampilandata() {
-	cout << "===================================";
-	cout << "Nama" << "\t\t" << "Status" << endl;
-	cout << "===================================";
-	for (int i = 0; i < 20; i++) {
-		cout << nama[i] << "\t\t" << status(rataNil[i], nilM[i]);
-	}
+    cout << "=======================================" << endl;
+    cout << "Nama\t\tStatus" << endl;
+    cout << "=======================================" << endl;
+    for (int i = 0; i < 20; i++) {
+        cout << nama[i] << "\t\t" << status(rataNil[i], nilM[i]) << endl;
+
+    }
 }
 
 void kandidatditerima() {
-	for (int i = 0; i < 20; i++) {
-		if (status(rataNil[i], nilM[i]) == "Diterima"){
-			jumlahditerima++;
-		}else {
-			jumlahditolak++;
-		}
-	}
-
+    int jumlahDiterima = 0;
+    int jumlahDitolak = 0;
+    for (int i = 0; i < 20; i++) {
+        if (status(rataNil[i], nilM[i]) == "Diterima") {
+            jumlahDiterima++;
+        }
+        else {
+            jumlahDitolak++;
+        }
+    }
+    cout << "========================================" << endl;
+    cout << "Jumlah kandidat diterima : " << jumlahDiterima << endl;
+    cout << "Jumlah kandidat tidak diterima : " << jumlahDitolak << endl;
+    cout << "========================================" << endl;
 }
 
 int main() {
-	void input();
-	void tapilandata();
-	void kandidatditerima();
-	return 0;
+    inputdata();
+    tampilandata();
+    kandidatditerima();
+    return 0;
 }
